731eed2487c9239c18a2c733a2cb343969f673dc
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-20 22:32:32 +0530

    Question - 7

---
+++ JavaScript Assignment/07-last-index-of.js
@@ -0,0 +1,5 @@
+// 07. Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+let arr = ["pw", "ineuron", "school", "skills"];
+
+console.log("first occurance pw : ", arr.lastIndexOf("pw"));
+console.log("last occurance skills : ", arr.lastIndexOf("skills"));
