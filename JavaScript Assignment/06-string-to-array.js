357dedbc6d469ddc5a60e2389102cd91e25fc245
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-20 22:18:43 +0530

    Question - 6

---
+++ JavaScript Assignment/06-string-to-array.js
@@ -0,0 +1,5 @@
+// 06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+let str = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+let strArr = str.split(",");
+console.log(strArr);
