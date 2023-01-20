47b9018e912259c53e9af854a400dc7825690e3a
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-20 22:34:06 +0530

    Question - 8

---
+++ JavaScript Assignment/08-trim.js
@@ -0,0 +1,5 @@
+// 08. Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+let str = "   iNeuron     ";
+console.log(str);
+console.log(str.trim());
