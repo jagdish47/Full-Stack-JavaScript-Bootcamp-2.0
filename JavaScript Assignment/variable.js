9060aa80a9982ca9b322d523124aa4f4524e0770
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-20 21:56:12 +0530

    question 1

---
+++ JavaScript Assignment/variable.js
@@ -0,0 +1,13 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+let str = "Jagdish";
+let isHuman = true;
+let value = undefined;
+let type = null;
+let age = 21;
+
+console.log("datatype : ", typeof str, ", value : ", str);
+console.log("datatype : ", typeof isHuman, ", value : ", isHuman);
+console.log("datatype : ", typeof value, ", value : ", value);
+console.log("datatype : ", typeof type, ", value : ", type);
+console.log("datatype : ", typeof age, ", value : ", age);
