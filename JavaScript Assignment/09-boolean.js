ffd1a8a6b19ad360e4752e309994f3c82672bd3e
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-20 23:01:37 +0530

    Question - 9

---
+++ JavaScript Assignment/09-boolean.js
@@ -0,0 +1,15 @@
+// 09. Boolean value is either true or false.
+//     - Write three JavaScript statement example which provide truthy value.
+//     - Write three JavaScript statement example which provide falsy value.
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+console.log(a > b);
+console.log(a > c);
+console.log(b > c);
+
+console.log(a < b);
+console.log(a < c);
+console.log(b < c);
