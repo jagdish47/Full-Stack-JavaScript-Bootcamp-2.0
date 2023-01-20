08a787ab233bebe1c74f9438d40daf7f5656d2a7
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-20 22:05:33 +0530

    question - 2

---
+++ JavaScript Assignment/interploation-method.js
@@ -0,0 +1,11 @@
+// Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+let firstName = "Jagdish";
+let lastName = "Kumawat";
+let maritalStatus = "Single";
+let country = "India";
+let age = 21;
+
+console.log(
+  `Hey my name is ${firstName} ${lastName} and I'm ${maritalStatus}, I am from ${country} and I'm ${age} years old`
+);
