f21f9d8e8fb40698f8bc03dcdfb76e4e0d9bb263
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-20 22:13:05 +0530

    Question - 4

---
+++ JavaScript Assignment/04-include.js
@@ -0,0 +1,6 @@
+// 04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+let str = "Hello my name is Jagdish";
+
+console.log(str.includes("Jagdish"));
+console.log(str.includes("jagdish"));
