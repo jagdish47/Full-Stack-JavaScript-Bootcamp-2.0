95ab47ecfe004dd10107b9191b3c93b3312d05f8
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-20 22:15:31 +0530

    Question - 5

---
+++ JavaScript Assignment/05-split.js
@@ -0,0 +1,6 @@
+// 05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+let str = "JAGDISH";
+
+let strArr = str.split("");
+console.log(strArr);
