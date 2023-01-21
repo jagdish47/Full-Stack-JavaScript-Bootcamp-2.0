e2deb9681b27c25993b3cca6df0b2620fecc452f
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-21 13:18:58 +0530

    Question - 14

---
+++ JavaScript Assignment/14-even-odd.js
@@ -0,0 +1,9 @@
+// 14. Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript? Create a program which checks that the given number is even or odd.
+
+let digit = 10;
+
+if (digit % 2 == 0) {
+  console.log("Even number");
+} else {
+  console.log("Odd number");
+}
