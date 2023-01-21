58d07e812d4ebbfb03ee3f2ab105ae98080f2b55
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-21 13:12:46 +0530

    Question - 12

--- JavaScript Assignment/12-date-format.js
+++ JavaScript Assignment/12-date-format.js
@@ -2,3 +2,15 @@
 //     - YYYY-MM-DD HH:mm
 //     - DD-MM-YYYY HH:mm
 //     - DD/MM/YYYY HH:mm
+
+let date = new Date();
+
+let day = date.getDay();
+let mon = date.getMonth();
+let year = date.getFullYear();
+let hour = date.getHours();
+let min = date.getMinutes();
+
+console.log(`${year}-${mon + 1}-${day} ${hour}:${min}`);
+console.log(`${day}-${mon}-${year} ${hour}:${min}`);
+console.log(`${day}/${mon}/${year} ${hour}:${min}`);
