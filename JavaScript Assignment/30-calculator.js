4dd79b2ff31a5a74d78bb46e9699e362442c6806
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-01-23 16:42:44 +0530

    Question - 30

---
+++ JavaScript Assignment/30-calculator.js
@@ -0,0 +1,17 @@
+// Create a simple calculator program in JavaScript which can perform the addition, substraction, multiplication and division on given numbers.
+
+function addition(a, b) {
+  return a + b;
+}
+
+function substraction(a, b) {
+  return a - b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
