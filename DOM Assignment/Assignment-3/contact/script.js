4f03372df2ab2f83fd4f70b3b12ca997fd354a5f
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-02-05 09:51:37 +0530

    Assignment 3 complete

---
+++ DOM Assignment/Assignment-3/contact/script.js
@@ -0,0 +1,17 @@
+const name = document.getElementsByClassName("enterName");
+name[0].placeholder = "FSJS 2.0";
+
+const email = document.getElementsByClassName("enterMail");
+email[0].placeholder = "fsja@ineuron.ai";
+
+const message = document.getElementsByClassName("enterMessage");
+message[0].placeholder = "Hello World";
+
+const userName = document.getElementsByClassName("userName");
+userName[0].placeholder = "FSJS 2.0";
+
+const userEmail = document.getElementsByClassName("userEmail");
+userEmail[0].placeholder = "fsja@ineuron.ai";
+
+const userMessage = document.getElementsByClassName("userMessage");
+userMessage[0].placeholder = "Hello World";
